4182fdd0916fa6aee0422e8d9aab74558be369fd
Ravi Kant gupta <ravijiro@gmail.com>
2026-07-24 11:37:40 +0530

    Added sitemap and robots

---
+++ app/sitemap.js
@@ -0,0 +1,21 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://japaneselanguagecourse.com",
+      lastModified: new Date(),
+      priority: 1,
+    },
+
+    {
+      url: "https://japaneselanguagecourse.com/contact",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+
+    {
+      url: "https://japaneselanguagecourse.com/courses",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+  ];
+}
